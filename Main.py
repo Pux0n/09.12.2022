3c0b15dc0d9a8c576cfb6ef409943fa4b794316b
uczen <uczen@ckziu.net>
2022-12-09 11:15:35 +0100

    Adding Binary function

--- Main.py
+++ Main.py
@@ -1,2 +1,18 @@
 x = float(input("Podaj x: "))
 k = int(input("Podaj k: "))
+
+def binary(x, k):
+    print("0,", end='')
+    y = x
+    for i in range(k):
+        if y >= 1/2:
+            print("1", end='')
+        else:
+            print("0", end='')
+        y = y*2
+        if y >= 1:
+            y = y-1
+
+
+binary(x, k)
+
