da7e122da88834de49be023faa5b0a752b876a96
Pux0n <120049228+Pux0n@users.noreply.github.com>
2022-12-09 13:42:47 +0100

    Added Classes
    Added Classes

--- Main.py
+++ Main.py
@@ -1,36 +1,50 @@
-x = float(input("Podaj x: "))
-k = int(input("Podaj k: "))
-
-def binary(x, k):
-    print("0,", end='')
-    y = x
-    for i in range(k):
-        if y >= 1/2:
-            print("1", end='')
-        else:
-            print("0", end='')
-        y = y*2
-        if y >= 1:
-            y = y-1
+number = float(input("Podaj x: "))
+accuracy = int(input("Podaj k: "))
 
 
-def trinary(x, k):
-    print("0,", end='')
-    y = x
-    for i in range(k):
-        if y >= 2/3:
-            print("2", end='')
-        elif y >= 2/9:
-            print("1", end='')
-        elif y >= 2/27:
-            print("0", end='')
-        y = y * 3
-        if y >= 2:
-            y = y - 2
-        elif y >= 1:
-            y = y - 1
+class Binary_Class:
+    x = number
+    k = accuracy
+
+    def binary(self, x, k):
+        print("0,", end='')
+        y = x
+        for i in range(k):
+            if y >= 1/2:
+                print("1", end='')
+            else:
+                print("0", end='')
+            y = y*2
+            if y >= 1:
+                y = y-1
 
 
-binary(x, k)
+bin = Binary_Class()
+
+
+class Trinary_Class:
+    x = number
+    k = accuracy
+
+    def trinary(self, x, k):
+        print("0,", end='')
+        y = x
+        for i in range(k):
+            if y >= 2/3:
+                print("2", end='')
+            elif y >= 2/9:
+                print("1", end='')
+            elif y >= 2/27:
+                print("0", end='')
+            y = y * 3
+            if y >= 2:
+                y = y - 2
+            elif y >= 1:
+                y = y - 1
+
+
+tri = Trinary_Class()
+
+bin.binary(number, accuracy)
 print(" ", end='')
-trinary(x, k)
+tri.trinary(number, accuracy)
